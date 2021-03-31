db4296a015d007f6e0af29aaa95cbbd46e829747
Saheed Ogunsolu <saheedogunsolu2016@gmail.com>
2021-03-31 12:38:30 +0100

    added all the questions

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-var toggle = document.getElementById("btn-toggle");
-const sun = document.querySelector(".sun");
-const moon = document.querySelector(".moon");
+const toggle = document.getElementById("btn-toggle");
+const lightModeBtn = document.querySelector(".light-mode-button");
+const darkModeBtn = document.querySelector(".dark-mode-button");
 
-var storedTheme =
-  localStorage.getItem("theme") ||
-  (window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light");
+const getStoredTheme = () => {
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    return storedTheme;
+  }
+  const mediaMatches = window.matchMedia("(prefers-color-scheme: dark)")
+    .matches;
+  if (mediaMatches) {
+    return "dark";
+  }
+  return "light";
+};
+
+const storedTheme = getStoredTheme();
 if (storedTheme)
   document.documentElement.setAttribute("data-theme", storedTheme);
 
-toggle.onclick = function () {
+toggle.addEventListener("click", addingClasses);
+
+function addingClasses() {
   var currentTheme = document.documentElement.getAttribute("data-theme");
-  var targetTheme = "light";
+  let targetTheme = "light";
 
   if (currentTheme === "light") {
     targetTheme = "dark";
-    moon.classList.add("d-none");
-    sun.classList.remove("d-none");
+    darkModeBtn.classList.add("d-none");
+    lightModeBtn.classList.remove("d-none");
   } else {
-    sun.classList.add("d-none");
-    moon.classList.remove("d-none");
+    lightModeBtn.classList.add("d-none");
+    darkModeBtn.classList.remove("d-none");
   }
 
   document.documentElement.setAttribute("data-theme", targetTheme);
   localStorage.setItem("theme", targetTheme);
-};
+}
